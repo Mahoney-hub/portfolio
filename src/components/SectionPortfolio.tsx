3f7d0e062be756fc387574e7b9a72fe7998d5752
Pavel Vasilev <pv9998286620@yandex.ru>
2022-03-19 17:37:47 +0300

    added project Unit in portfolio

--- src/components/SectionPortfolio.tsx
+++ src/components/SectionPortfolio.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import budgetCalculator from '../images/bg-portfolio/budget-calculator.jpg';
+import projectUnit from '../images/bg-portfolio/project-unit.jpg';
 import orange from '../images/bg-portfolio/orange.jpg';
 import telephone from '../images/bg-portfolio/telephone.jpg';
 import coffee from '../images/bg-portfolio/coffee.jpg';
@@ -13,22 +14,22 @@ export const SectionPortfolio = () => {
             <div className="box-container">
 
                 <div className="box">
-                    <img src={budgetCalculator} alt="image"/>
+                    <img src={projectUnit} alt="image"/>
                     <div className="content">
-                        <h3>budget calculator</h3>
-                        <p>Project budget calculator</p>
-                        <p>Technologies: SCSS, React TS, Redux-Toolkit</p>
-                        <a href="https://Mahoney-hub.github.io/budget_calculator" target="_blank">view the
+                        <h3>Unit</h3>
+                        <p>Technologies: SCSS, JS, Jquery</p>
+                        <a href="https://mahoney-hub.github.io/project-unit/" target="_blank">view the
                             project</a>
                     </div>
                 </div>
 
                 <div className="box">
-                    <img src={orange} alt="image"/>
+                    <img src={budgetCalculator} alt="image"/>
                     <div className="content">
-                        <h3>project 02</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, odit!</p>
-                        <a href="#">read more</a>
+                        <h3>budget calculator</h3>
+                        <p>Technologies: SCSS, React TS, Redux-Toolkit</p>
+                        <a href="https://Mahoney-hub.github.io/budget_calculator" target="_blank">view the
+                            project</a>
                     </div>
                 </div>
 
